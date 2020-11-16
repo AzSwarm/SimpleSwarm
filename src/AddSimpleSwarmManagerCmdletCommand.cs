18c633c1f6be93415e472452f7378792529359eb
aespana <aespana@applaudostudios.com>
2020-11-16 14:13:56 -0600

    Initial Commit

---
+++ src/AddSimpleSwarmManagerCmdletCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace SimpleSwarm
+{
+    [Cmdlet("Add", "SimpleSwarmManager")]
+    public class AddSimpleClusterManagerCmdletCommand : PSCmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Begin!");
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            WriteObject("Processing!");
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            WriteVerbose("End!");
+        }
+    }
+}
