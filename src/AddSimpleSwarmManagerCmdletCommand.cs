26cf553982f30d19b87b62ad4e9813566a2b21a8
aespana <aespana@applaudostudios.com>
2020-11-23 15:32:15 -0600

    Adding Microsoft.Azure.Management.ResourceManager.Fluent dependency and get credential using AZURE_AUTH_LOCATION environment variable and azureauth.json

--- src/AddSimpleSwarmManagerCmdletCommand.cs
+++ src/AddSimpleSwarmManagerCmdletCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Management.Automation;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
 
 namespace SimpleSwarm
 {
@@ -15,6 +16,13 @@ namespace SimpleSwarm
         protected override void ProcessRecord()
         {
             WriteObject("Processing!");
+
+            var credentials = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+
+            var azure = Microsoft.Azure.Management.Fluent.Azure
+                .Configure()
+                .Authenticate(credentials)
+                .WithDefaultSubscription();
         }
 
         // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
